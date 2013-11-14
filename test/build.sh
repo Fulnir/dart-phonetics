a6df12c7e38495f6d3f137297ed79895c4510b8b
Edwin Bühler <edbuehler@me.com>
2013-11-14 20:07:57 +0100

    dart.sdk path changed in build.sh

--- test/build.sh
+++ test/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd $WORKSPACE
-export DART_SDK=/Users/Shared/dart-sdk
+export DART_SDK=/Users/Shared/dart/dart-sdk
 export PATH=$PATH:$DART_SDK/bin
 pub install
 dartdoc --no-show-private --link-api --pkg $WORKSPACE/packages/ $WORKSPACE/lib/*.dart
