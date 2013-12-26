8849d7d0309e44c654d40444707280a2e1956704
Edwin Bühler <edbuehler@icloud.com>
2013-12-26 13:57:39 +0100

    excludeLibs in hop runner

--- tool/hop_runner.dart
+++ tool/hop_runner.dart
@@ -1,7 +1,7 @@
 //
 // $ dart tool/hop_runner.dart --color docs --allow-dirty
 //
-library sparse_list.hop_runner;
+library phonetics.hop_runner;
 
 
 import 'package:hop/hop.dart';
@@ -10,7 +10,12 @@ import 'package:hop/hop_tasks.dart';
 void main(List<String> args) {
 
 
-  addTask('docs', createDartDocTask(['lib/phonetics.dart']));
+  addTask('docs', createDartDocTask(['lib/phonetics.dart'],
+      linkApi: true,
+      excludeLibs: ['intl', 'logging', 'dart.pkg.collection.wrappers',
+                    'dart.collection_helpers.wrappers', 'number_symbol_data',
+                    'number_symbols', 'intl_helpers', 'date_symbols',
+                    'date_format_internal']));
 
   runHop(args, paranoid: false);
 }
